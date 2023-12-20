c8b4c8d4096eb0a5b9abdfb80ac50fa3ab5547cc
Clement Macharia <clementmacharia62@gmail.com>
2023-12-20 23:28:44 +0300

    complete

--- lib/seed.sql
+++ lib/seed.sql
@@ -9,26 +9,12 @@ INSERT INTO bears (id, name, age, sex, color, temperament, alive) VALUES
 (8,null, 20, "M", "black", "aggressive", 0);
 
 
-SELECT name,age
-FROM bears 
-WHERE sex = "F";
 
-SELECT * 
-FROM bears 
-ORDER BY name ASC;
 
-SELECT name, age
-FROM bears
-WHERE alive = 1
-ORDER BY age ASC
 
-SELECT name,age
-FROM bears
-ORDER BY age DESC
-LIMIT 1;
 
-SELECT name, age
-FROM bears
-ORDER BY age ASC
-LIMIT 1;
+
+
+
+
 
